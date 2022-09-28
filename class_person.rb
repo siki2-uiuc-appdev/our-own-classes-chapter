3b692fe01fc68ca367b85ef24cbc66b2e5fb54a1
siki2-uiuc <siki2@illinois.edu>
2022-09-28 19:43:37 +0000

    chapter completed

--- class_person.rb
+++ class_person.rb
@@ -7,7 +7,25 @@
 #  - An instance method, age, that calculates the number of years between today and birthdate
 
 class Person
-  
+  require("date")
+
+  attr_accessor :first_name
+  attr_accessor :last_name
+  attr_accessor :birthdate
+
+  def full_name
+    return "#{self.first_name} #{self.last_name}"
+  end
+
+  def age
+    date_of_birth = Date.parse(self.birthdate)
+    today = Date.today
+    age_in_days = today - date_of_birth
+    age_in_years = age_in_days / 365
+
+    return age_in_years.to_i
+  end
+
 end
 
 # Test your instance methods by adding to the end of this file:
